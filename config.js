abd6c10e2627c03ed6440540df0a766e08139038
Tuanlailaptrinh <87818411+Tuanlailaptrinh@users.noreply.github.com>
2022-06-08 22:16:37 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Làm nguời yêu anh nhé <33',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Đồng Ý :>>',
+    btnNo: 'Không :(',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Cảm ơn bbi nhiều lắm',
+    messDesc: 'Yêu em nhắm bbi',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://m.me/thaituan.work' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
